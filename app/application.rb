b8525f1452c1edf976d64bb2dae96d31bc7da09a
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-12-08 05:11:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,12 +23,24 @@ class Application
           resp.write "#{cart_item}\n"
         end
       end
+      
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      if @@items.include?(item)
+        @@cart << item
+        resp.write "added #{item}"
+      else
+        resp.write "We don't have that item"
+      end
+      
     else
       resp.write "Path Not Found"
     end
 
     resp.finish
   end
+
+
 
   def handle_search(search_term)
     if @@items.include?(search_term)
